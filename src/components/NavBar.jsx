894f733163b5ab4f537039528a4cf3c9f9874331
bckflpboys <crushadde@gmail.com>
2025-02-02 16:31:43 +0200

    fixed navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,13 +35,13 @@ const NavBar = () => {
                     <nav className="hidden md:block">
                         <ul className="flex flex-row space-x-4 p-4 font-semibold">
                             <li>
-                                <Link to="/#features" className="text-grey-600">Features</Link>
+                                <a href="#features" className="text-grey-600">Features</a>
                             </li>
                             <li>
-                                <Link to="/#projects" className="text-grey-600">Projects</Link>
+                                <a href="#projects" className="text-grey-600">Projects</a>
                             </li>
                             <li>
-                                <Link to="/#pricing" className="text-grey-600">Pricing</Link>
+                                <a href="#pricing" className="text-grey-600">Pricing</a>
                             </li>
                             <li>
                                 <Link 
@@ -54,7 +54,7 @@ const NavBar = () => {
                         </ul>
                     </nav>
                     <div className="hidden md:block">
-                        <Link to="/#contact" className="bg-black px-4 py-2 rounded-md text-white cursor-pointer ml-2">Contact</Link>
+                        <a href="#contact" className="bg-black px-4 py-2 rounded-md text-white cursor-pointer ml-2">Contact</a>
                     </div>
                     <MobileMenu />
                 </div>
